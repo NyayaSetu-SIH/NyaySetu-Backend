e23dc34c575cd6a2a7a0d7da0381c851ecc4cdc9
mayankmj <mayankjohari877@gmail.com>
2023-12-20 17:29:48 +0530

    .

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json({extended: true}));
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(
     cors({
-        origin: "https://https://nyaysetu.netlify.app/",
+        origin: "https://nyaysetu.netlify.app/",
         methods:"GET,PUT,POST,DELETE",
         credentials:true
     })
