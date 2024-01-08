4b2942312b08c233d18a292f9bfc854f4e5dbafe
mayankmj <mayankjohari877@gmail.com>
2024-01-09 03:04:35 +0530

    .

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json({extended: true}));
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin:`${process.env.CLIENT_URL}`,
         methods:"GET,PUT,POST,DELETE",
         credentials:true
     })
